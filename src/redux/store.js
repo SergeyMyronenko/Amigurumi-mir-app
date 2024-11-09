814aa5362de94ad08de773119ec05d3ac7fc216e
Sergiy Myronenko <Sergiymyronenko.web@gmail.com>
2024-11-09 14:48:28 +0200

    init project

---
+++ src/redux/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+
+// const userPersistConfig = {
+//   key: "user",
+//   storage,
+//   whitelist: ["refreshToken"], // вказати яку властивість зберігти, поки що додав тільки токен
+// };
+// const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);

c3f4d3e844facd982ebc74e832fd95c3810a37d9
Sergiy Myronenko <Sergiymyronenko.web@gmail.com>
2024-11-11 13:35:36 +0200

    add style to navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,83 @@
 import css from "./Header.module.css";
 import { Navigation } from "../Navigation/Navigation";
 import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+import facebook from "../../images/facebook.png";
+import instagram from "../../images/instagram.png";
 
 export const Header = () => {
   return (
     <div className={css.header}>
-      <p>empty blok header</p>
+      <div className={css.headerIcon}>
+        <a
+          className={css.headerIconLink}
+          href="https://www.facebook.com/AmigurumiMir/"
+          target="_blank"
+        >
+          <img
+            className={css.headerIconImage}
+            src={facebook}
+            alt="icon facebook"
+            width="20"
+            height="20"
+          />
+        </a>
+        <a
+          className={css.headerIconLink}
+          href="https://www.instagram.com/amigurumi_mir_/"
+          target="_blank"
+        >
+          <img
+            className={css.headerIconImage}
+            src={instagram}
+            alt="icon instagram"
+            width="20"
+            height="20"
+          />
+        </a>
+        {/* <Link
+          className={css.headerIconLink}
+          to="https://www.facebook.com/AmigurumiMir/"
+        >
+          <img
+            className={css.headerIconImage}
+            src={facebook}
+            alt="icon facebook"
+            width="20"
+            height="20"
+          />
+        </Link>
+        <Link
+          className={css.headerIconLink}
+          onClick={(e) => {
+            e.preventDefault();
+            window.open(
+              "https://www.instagram.com/amigurumi_mir_/",
+              "_blank",
+              "noopener,noreferrer"
+            );
+          }}
+        >
+          <img
+            className={css.headerIconImage}
+            src={instagram}
+            alt="icon instagram"
+            width="20"
+            height="20"
+          />
+        </Link> */}
+      </div>
       <div className={css.navi}>
-        <img src={logo} alt="" width="60" height="60" />
+        <div className={css.naviLogo}>
+          <img src={logo} alt="" width="60" height="60" />
+          <div>
+            <Link to="/Amigurumi-mir" className={css.logoLink}>
+              Amigurumi-mir
+            </Link>
+            <p className={css.desc}>Пряжа та товари для рукоділля</p>
+          </div>
+        </div>
+
         <Navigation />
       </div>
     </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,84 @@
 import { Link } from "react-router-dom";
 import css from "./Navigation.module.css";
+import { useState } from "react";
+import clsx from "clsx";
 
 export const Navigation = () => {
+  const [activeLink, setActiveLink] = useState("Головна");
+
+  const handleSelectedLink = (title) => {
+    setActiveLink(title);
+  };
+
   return (
-    <div>
+    <div className={css.naviWrapper}>
       <nav className={css.navi}>
         <ul className={css.naviList}>
-          <li>
-            <Link to="/Amigurumi-mir/">Головна</Link>
+          <li
+            className={clsx(
+              css.naviListItem,
+              activeLink === "Головна" && css.active
+            )}
+            onClick={() => {
+              handleSelectedLink("Головна");
+            }}
+          >
+            <Link to="/Amigurumi-mir/" className={css.naviListLink}>
+              Головна
+            </Link>
           </li>
-          <li>
-            <Link to="/Amigurumi-mir/catalog">Каталог</Link>
+          <li
+            className={clsx(
+              css.naviListItem,
+              activeLink === "Каталог" && css.active
+            )}
+            onClick={() => {
+              handleSelectedLink("Каталог");
+            }}
+          >
+            <Link to="/Amigurumi-mir/catalog" className={css.naviListLink}>
+              Каталог
+            </Link>
           </li>
-          <li>
-            <Link to="/Amigurumi-mir/about-us">Про Нас</Link>
+          <li
+            className={clsx(
+              css.naviListItem,
+              activeLink === "Про Нас" && css.active
+            )}
+            onClick={() => {
+              handleSelectedLink("Про Нас");
+            }}
+          >
+            <Link to="/Amigurumi-mir/about-us" className={css.naviListLink}>
+              Про Нас
+            </Link>
           </li>
-          <li>
-            <Link to="/Amigurumi-mir/payment-and-delivery">
+          <li className={css.naviListItem}>
+            <Link
+              to="/Amigurumi-mir/payment-and-delivery"
+              className={clsx(
+                css.naviListLink,
+                activeLink === "Оплата та доставка" && css.active
+              )}
+              onClick={() => {
+                handleSelectedLink("Оплата та доставка");
+              }}
+            >
               Оплата та доставка
             </Link>
           </li>
-          <li>
-            <Link to="/Amigurumi-mir/contacts">Контакти</Link>
+          <li
+            className={clsx(
+              css.naviListItem,
+              activeLink === "Контакти" && css.active
+            )}
+            onClick={() => {
+              handleSelectedLink("Контакти");
+            }}
+          >
+            <Link to="/Amigurumi-mir/contacts" className={css.naviListLink}>
+              Контакти
+            </Link>
           </li>
         </ul>
       </nav>
